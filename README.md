41cdd6ec2afbe1e6f638be7fece093c9ff95821b
Gindra-o7 <gilangindra734@gmail.com>
2025-11-26 22:06:42 +0700

    refactor: Update project name and messages to reflect new branding for PMB STIE Mahardhika

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# hono-api-boilerplate
+# hono-api-pmb-stie-mahardhika
 This repository contains the backend code for Internship Administration Service. Assembled with Hono TS + Prisma ORM (PostgreSQL), it manages the core logic, data handling, and secure processing of internship data. Essential for supporting the academic requirements at UIN Suska Riau.
 
 # how to running:
--- src/services/global.service.ts
+++ src/services/global.service.ts
@@ -5,7 +5,7 @@ export default class GlobalService {
     public static async introduce(): Promise<GlobalServiceIntroduceResponse> {
         return {
             'response': true,
-            'message': 'Cihuy, Halow Semua 👋 ~ Selamat datang di API Boilerplate! 🎉',
+            'message': 'Cihuy, Halow Semua 👋 ~ Selamat datang di API PMB STIE Mahardhika! 🎉',
             'version': process.env.APP_VERSION || '1.0.0',
             'contributor': 'https://github.com/Gindra-o7/hono-api-boilerplate',
             'timezone': `Asia/Jakarta ~ ${new Date().toLocaleString('id-ID', { timeZone: 'Asia/Jakarta' })} WIB`
@@ -15,7 +15,7 @@ export default class GlobalService {
     public static async health(): Promise<GlobalServiceHealthResponse> {
         return {
             'response': true,
-            'message': 'Cihuy, API Boilerplate sehat-sehat saja! 😁',
+            'message': 'Cihuy, API PMB STIE Mahardhika sehat-sehat saja! 😁',
             'status': 'OK',
             'uptime': process.uptime(),
             'memoryUsage': process.memoryUsage()
